7c3aa8f6de00ac0c1dc06fe267478314ad2a62f0
Philipp Richter <phlpp.rchtr@protonmail.com>
2018-10-10 11:22:06 +0200

    deploy.js edited - contract working

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -13,8 +13,8 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
-    .send({ gas: '1000000', from: accounts[0] });
+    .deploy({ data: '0x' + bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
