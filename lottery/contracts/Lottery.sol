d26eb828f420acba2262bd81f8d9a86e28abc226
Philipp Richter <phlpp.rchtr@protonmail.com>
2018-10-12 15:37:19 +0200

    lottery added

--- lottery/contracts/Lottery.sol
+++ lottery/contracts/Lottery.sol
@@ -29,7 +29,7 @@ contract Lottery {
         _;
     }
     
-    function getPlayer() public view returns (address[]) {
+    function getPlayers() public view returns (address[]) {
         return players;
     }
 }
